c2bd80f96be06ea206f074da78a6eef0081785ee
gyf <gyf@travelsky.com.cn>
2023-01-30 14:56:24 +0800

    init

---
+++ src/main/java/org/phial/baas/BaasApplication.java
@@ -0,0 +1,13 @@
+package org.phial.baas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/phial/baas/BaasApplicationTests.java
@@ -0,0 +1,13 @@
+package org.phial.baas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
