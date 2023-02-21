591b6dcb2b81d7363026ff61586f1eedc7187fa1
gyf <gyf@travelsky.com.cn>
2023-02-21 09:53:53 +0800

    update

--- baas-manager/src/main/java/org/phial/baas/manager/config/filter/CrosFilter.java
+++ baas-manager/src/main/java/org/phial/baas/manager/config/filter/CrosFilter.java
@@ -1,6 +1,11 @@
 package org.phial.baas.manager.config.filter;
 
+import cn.hutool.json.JSONUtil;
+import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.phial.baas.manager.config.source.DynamicDataSourceContextHolder;
+import org.springframework.http.ResponseEntity;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -11,6 +16,8 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Optional;
 
 
 /**
@@ -20,16 +27,37 @@ import java.io.IOException;
 public class CrosFilter implements Filter {
 
     @Override
-    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-        HttpServletRequest req1 = (HttpServletRequest) req;
-        HttpServletResponse response = (HttpServletResponse) res;
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+        resp.setHeader("Access-Control-Allow-Origin", req.getHeader("Origin"));
+        resp.setHeader("Access-Control-Expose-Headers", "*");
+        resp.setHeader("Access-Control-Allow-Headers", "*");
+        resp.setHeader("Access-Control-Allow-Methods", "*");
+        resp.setHeader("Access-Control-Allow-Credentials", "true");
 
-        if ("OPTIONS".equals(((HttpServletRequest) req).getMethod())) {
-            response.setStatus(HttpServletResponse.SC_OK);
+        if ("OPTIONS".equals(req.getMethod())) {
+            resp.setStatus(HttpServletResponse.SC_OK);
             return;
         }
 
-        chain.doFilter(req, res);
+        String source = req.getHeader("source");
+        log.info("EnvFilter source:{}, [{} {}], params:{}", source, req.getMethod(), req.getRequestURL(), JSONUtil.toJsonStr(req.getParameterMap()));
+
+        if (StringUtils.isBlank(source)) {
+            resp.setCharacterEncoding("utf-8");
+            resp.setContentType("application/json; charset=utf-8");
+            PrintWriter writer = resp.getWriter();
+            ResponseEntity<String> res = ResponseEntity.of(Optional.of("source not found error"));
+            log.error("source:{} not found error!", source);
+            writer.write(JSON.toJSONString(res));
+            return;
+        }
+
+        // set source
+        DynamicDataSourceContextHolder.setCurrentSource(source);
+
+        chain.doFilter(req, resp);
     }
 
     @Override
--- baas-manager/src/main/java/org/phial/baas/manager/config/filter/FilterConfig.java
+++ baas-manager/src/main/java/org/phial/baas/manager/config/filter/FilterConfig.java
@@ -11,15 +11,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class FilterConfig {
 
-    @Bean("envFilter")
-    public FilterRegistrationBean envFilterRegistration() {
-        FilterRegistrationBean<EnvFilter> registration = new FilterRegistrationBean<>();
-        registration.setFilter(new EnvFilter());
-        registration.addUrlPatterns("/*");
-        registration.setOrder(-1);
-        return registration;
-    }
-
     @Bean("corsFilter")
     public FilterRegistrationBean crosFilteRegistration() {
         FilterRegistrationBean<CrosFilter> registration = new FilterRegistrationBean<>();
