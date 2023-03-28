41b52d54503b1456d2f0e974b777c94ee8ccd4af
gyf <gyf@travelsky.com.cn>
2023-03-28 20:53:38 +0800

    .

--- baas-service/src/main/java/org/phial/baas/service/service/bc/ChainNodeService.java
+++ baas-service/src/main/java/org/phial/baas/service/service/bc/ChainNodeService.java
@@ -1,12 +1,7 @@
 package org.phial.baas.service.service.bc;
 
 
-import org.phial.baas.service.domain.entity.bc.ChainNode;
-
-import java.util.List;
-
 public interface ChainNodeService {
 
-    List<ChainNode> listAllByCondition(ChainNode beanCondition);
 
 }
